3942d2c43c48e0d5f92469c1e4e99077d124568c
bhivert <hivert.benoit@gmail.com>
2017-07-28 04:26:50 +0200

    main site basis && config file

---
+++ sites/main/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * File: sites/main/config.php
+ * @author Benoit HIVERT <hivert.benoit@gmail.com>
+ * Date: 20.07.2017
+ * Last Modified Date: 20.07.2017
+ * Last Modified By: Benoit HIVERT <hivert.benoit@gmail.com>
+ */
+
+return Array (
+	'errors'	=>	true,
+	'https'		=>	false,
+	'charset'	=>	'utf-8',
+	'title'		=>	'==> TITLE <==',
+	'author'	=>	'==> AUTHOR <==',
+	'email'		=>	'==> EMAIL <==',
+	'keywords'	=>	'==> KEYWORDS <==',
+	'description'	=>	'==> DESCRIPTION <=='
+);
