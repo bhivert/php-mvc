64d92dfe41c6475addab2d0f38b7807d4db3c98f
bhivert <hivert.benoit@gmail.com>
2017-07-28 04:28:37 +0200

    public folder basis

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * File: public/index.php
+ * @author Benoit HIVERT <hivert.benoit@gmail.com>
+ * Date: 20.07.2017
+ * Last Modified Date: 22.07.2017
+ * Last Modified By: Benoit HIVERT <hivert.benoit@gmail.com>
+ */
+
+try {
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	ini_set('error_reporting', E_ALL);
+
+	define('ROOT', dirname(__DIR__).'/');
+
+	require ROOT.'core/Autoloader.php';
+	\Core\Autoloader::register();
+
+	(new \Core\Router())->route($_SERVER['REQUEST_URI']);
+
+} catch (Exception $e) {
+	echo "{$e->getMessage()}<br />".PHP_EOL;
+}
+
