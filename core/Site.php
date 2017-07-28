db0f89287a143bce0c09daa6e852484c4ad3d3c9
bhivert <hivert.benoit@gmail.com>
2017-07-28 04:27:43 +0200

    core/site class

---
+++ core/Site.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * File: /home/noxx/00-www/php-mvc/core/Site.php
+ * @author Benoit HIVERT <hivert.benoit@gmail.com>
+ * Date: 20.07.2017
+ * Last Modified Date: 23.07.2017
+ * Last Modified By: Benoit HIVERT <hivert.benoit@gmail.com>
+ */
+
+namespace Core;
+
+class Site {
+	private	$_router;
+	private	$_folder;
+	private $_config;
+
+	function __construct(Rooter $router, $folder) {
+		$file = ROOT."sites/{$folder}/config.php";
+		if (!file_exists($file))
+			throw new \Exception("Config error: folder, '{$folder}' not found !");
+		$this->_router = $router;
+		$this->_folder = $folder;
+		$this->_config = require $file;
+	}
+
+	function getFolder() {
+		return $this->_folder;
+	}
+
+	function getErrors() {
+		if (!isset($this->_config['errors']) || $this->_config['errors'] !== true)
+			return false;
+		return true;
+	}
+
+	function getHttps() {
+		if (!isset($this->_config['https']) || $this->_config['errors'] !== true)
+			return false;
+		return true;
+	}
+
+	function getCharset() {
+		if (!isset($this->_config['charset']))
+			return 'utf-8';
+		return $this->_config['charset'];
+	}
+
+	function getTitle() {
+		if (!isset($this->_config['title']))
+			throw new \Exception("Config error: key, title not set !");
+		return $this->_config['title'];
+	}
+
+	function getAuthor() {
+		if (!isset($this->_config['author']))
+			throw new \Exception("Config error: key, author not set !");
+		return $this->_config['author'];
+	}
+
+	function getEmail() {
+		if (!isset($this->_config['email']))
+			throw new \Exception("Config error: key, email not set !");
+		return $this->_config['email'];
+	}
+
+	function getKeywords() {
+		return $this->_config['keywords'];
+	}
+
+	function getDescription() {
+		return $this->_config['description'];
+	}
+
+	function render(Array $kwargs) {
+		ob_start();
+		require ROOT."core/Templates/head.phtml";
+		$head = ob_get_clean();
+		ob_start();
+		ob_start();
+		foreach ($kwargs as $view => $content) {
+			$file = ROOT."sites/{$this->_folder}/views/"
+				.str_replace('.', '/', $view).".phtml";
+			if (!file_exists($file))
+				throw new \Exception("Render error: view: '{$view}' not found !");
+			require $file; 
+		}
+		$content = ob_get_clean();
+		require ROOT."core/Templates/body.phtml";
+		$body = ob_get_clean();
+		require ROOT."core/Templates/html.phtml";
+		exit ;
+	}
+}
