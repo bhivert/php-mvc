07381857fba35f1fa949ba90f614d34df6b63cfe
bhivert <hivert.benoit@gmail.com>
2017-09-07 06:53:39 +0200

    header update php-mvc/sites/main/database.php

--- sites/main/database.php
+++ sites/main/database.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * File              : database.php
+ * File              : sites/main/database.php
  * @author           : Benoit HIVERT <hivert.benoit@gmail.com>
- * Date              : 06.09.2017
- * Last Modified Date: 06.09.2017
+ * Date              : 07.09.2017
+ * Last Modified Date: 07.09.2017
  * Last Modified By  : Benoit HIVERT <hivert.benoit@gmail.com>
  */
 
